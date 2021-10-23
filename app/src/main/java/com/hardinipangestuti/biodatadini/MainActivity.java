062b3bfadb5b8545584cdbff9f89870c69fbbce4
BiodataDini <hardinipangestuti123@gmail.com>
2021-10-23 08:38:59 +0700

    BiodataDini

---
+++ app/src/main/java/com/hardinipangestuti/biodatadini/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hardinipangestuti.biodata;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
